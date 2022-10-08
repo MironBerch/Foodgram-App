905be7ba1aee78ba472e0d9261b0e3c883b0336a
MironBerch <mironberchetov@gmail.com>
2022-10-08 21:41:57 +0300

    clear

--- culinary_helper/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-
-admin.site.register(Profile)
-admin.site.register(Follower)
-admin.site.register(Recipe)
-admin.site.register(LikeRecipe)
-admin.site.register(RecipeReviews)
