c83822fac9b887de2d3fe9b41d8c1fa99fc63e76
MironBerch <mironberchetov@gmail.com>
2022-10-25 00:12:43 +0300

    Foodgram-App

--- foodgram/admin.py
+++ foodgram/admin.py
@@ -1,10 +1,5 @@
 from django.contrib import admin
-from foodgram.models import Recipe, Ingredient, RecipeIngredient, Follow, Favorites, Wishlist
+from foodgram.models import Ingredient
 
 
-admin.site.register(Recipe)
 admin.site.register(Ingredient)
-admin.site.register(RecipeIngredient)
-admin.site.register(Follow)
-admin.site.register(Favorites)
-admin.site.register(Wishlist)
